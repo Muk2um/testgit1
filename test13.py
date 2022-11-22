e26047ec789ebf861e14c3e903ec22be890df9e6
Mukesh <sizalmuky@gmail.com>
2022-11-22 10:50:50 +0530

    this is my first code

---
+++ test13.py
@@ -0,0 +1,13 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://Medata:8kPyvqDvfYWVKN4k@cluster0.zr2e2ig.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+d={
+    "name":"mukesh",
+    "lastname":"kumar",
+    "email_id":"sizalmuky@gmail.com"
+}
+db1=client['mongotest']
+coll=db['test']
+coll.insert_one(d)
